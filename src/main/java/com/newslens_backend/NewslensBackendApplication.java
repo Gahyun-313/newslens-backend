802a670b5f3979792fd90983966b17aaa285ab51
Gahyun-313 <gus20313@gmail.com>
2026-03-08 22:54:58 +0900

    chore(setup): 프로젝트 초기 설정

---
+++ src/main/java/com/newslens_backend/NewslensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.newslens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewslensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewslensBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newslens_backend/NewslensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newslens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewslensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
